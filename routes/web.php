5a1c3ac0f7288bd77f8950f05a0ae3dcaa1d4a2d
rovy-saputra-nugeraha <2101020012@student.umrah.ac.id>
2025-01-18 10:51:55 +0700

    Belajar Laravel 3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post; 
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home']);
@@ -10,8 +12,15 @@ Route::get('/about', function () {
     return view('about', ['title' => 'About Me']);
 });
 
-Route::get('/blog', function(){
-    return view('blog', ['title' => 'Blog Me']);
+Route::get('/posts', function(){
+    return view('posts', ['title' => 'Blog Me', 'posts' => Post::all()
+   ]);
+});
+
+Route::get('/posts/{post:slug}', function(Post $post){
+   // $post = Post::find($post);
+
+    return view('post', ['title' => 'Artikel Saya', 'post' => $post ]);
 });
 
 Route::get('/contact', function(){
